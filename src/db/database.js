86f5438a796ec5147c2f67848d78b50db9fa0506
Felipe García <126286822+Venom4545@users.noreply.github.com>
2025-05-09 18:37:18 -0500

    arreglo db

--- src/db/database.js
+++ src/db/database.js
@@ -1,16 +1,15 @@
-import mysql from "promise-mysql";
+import mysql from 'mysql2/promise';
+import config from './../config.js';
 
-import config from "./../config.js"
-
-const connection = mysql.createConnection({
-    host : config.host,
-    database : config.database,
+const connection = await mysql.createConnection({
+    host: config.host,
+    database: config.database,
     user: config.user,
     password: config.password
-})
+});
 
-const getConnection = () =>{
-    return connection
-}
+const getConnection = () => {
+    return connection;
+};
 
 export default getConnection;
